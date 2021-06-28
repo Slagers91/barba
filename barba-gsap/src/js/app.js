dbe3a71c1ef64e1b90b15ae7650cbb1e04fba54d
Ruud Slagers <ruudslagers@gmail.com>
2021-06-28 16:53:53 +0200

    barba.gsap

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,20 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+const animationEnter = (container) => {
+    return gsap.from(container, { autoAlpha: 0, duration: 2});
+}
+
+const animationLeave = (container) => {
+    return gsap.to(container, { autoAlpha: 0, duration: 2});
+}
+
+barba.init({
+    transitions: [
+        {
+            once(){
+                animationEnter(next.container);
+            }
+        }
+    ]
+})
